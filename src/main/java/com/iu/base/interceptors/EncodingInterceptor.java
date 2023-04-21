49602e4eb8a977f7907b2483931bec3627a97037
yangjinh94259 <yangjinh94259@gmail.com>
2023-04-21 15:34:04 +0900

    Spring Boot

--- src/main/java/com/iu/base/interceptors/EncodingInterceptor.java
+++ src/main/java/com/iu/base/interceptors/EncodingInterceptor.java
@@ -3,12 +3,23 @@ package com.iu.base.interceptors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.iu.base.board.notice.NoticeDAO;
+import com.iu.base.board.qna.QnaDAO;
 
 @Component
 public class EncodingInterceptor implements HandlerInterceptor{
 
+	@Autowired
+	private QnaDAO qnaDAO;
+	
+	@Autowired
+	private NoticeDAO noticeDAO;
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -17,6 +28,14 @@ public class EncodingInterceptor implements HandlerInterceptor{
 		
 		return true;
 		
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		//1. method
+		request.getMethod();
 	}
 	
 }
