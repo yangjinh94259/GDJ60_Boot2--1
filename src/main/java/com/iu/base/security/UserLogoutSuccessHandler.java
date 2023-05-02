0f9485cd0516cad93ffbb740098e92a602dcb014
yangjinh94259 <yangjinh94259@gmail.com>
2023-05-02 15:14:44 +0900

    0502

--- src/main/java/com/iu/base/security/UserLogoutSuccessHandler.java
+++ src/main/java/com/iu/base/security/UserLogoutSuccessHandler.java
@@ -47,7 +47,7 @@ public class UserLogoutSuccessHandler implements LogoutSuccessHandler{
 		
 		log.error("============Logout Success Handler============");
 		
-//		response.sendRedirect("https://kauth.kakao.com/oauth/logout?client_id="+restKey+"&logout_redirect_uri=http://localhost/");
+		response.sendRedirect("https://kauth.kakao.com/oauth/logout?client_id="+restKey+"&logout_redirect_uri=http://ec2-43-200-4-29.ap-northeast-2.compute.amazonaws.com/");
 		
 		
 //		response.sendRedirect("/");
